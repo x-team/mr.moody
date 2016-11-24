90e33ac3e283bb91ef0dd0c014f330d8581c65e8
Jacek Lawniczak <jacekelgda@gmail.com>
2016-11-24 14:03:27 +0200

    slash command to get reports of selected campaign

--- app/controllers/reportsController.js
+++ app/controllers/reportsController.js
@@ -1,10 +1,24 @@
 const reportsHandler = require('./../modules/reportsHandler')
+const chatHandler = require('./../modules/chatHandler')
 
-exports.generateReport = (req, res) => {
+generateCampaignsReport = (req, res) => {
   reportsHandler.getReportNames().then(reportsNames => {
     response = reportsNames.map(reportsHandler.getCampaignDate).map(function(reportDate, index) {
       return reportDate + ' : ' + reportsNames[index]
     })
     res.send('Available campaigns:' + response)
   })
+}
+
+generateCampaignReport = (campaignId, req, res) => {
+  reportsHandler.getCampaignReport().then(report => {
+    chatHandler.getEncodedReport(report).then(encodedReport => {
+      res.send(encodedReport)
+    })
+  })
+}
+
+module.exports = {
+  generateCampaignReport,
+  generateCampaignsReport
 }
