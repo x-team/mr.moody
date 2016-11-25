524ea17ed919a43bcd9aebd236f89b97a91fc2e4
Jacek Lawniczak <jacekelgda@gmail.com>
2016-11-25 10:09:49 +0200

    controllers require change

--- app/app.js
+++ app/app.js
@@ -4,15 +4,13 @@ const https = require('https')
 const fs = require('fs')
 const shortid = require('shortid')
 const app = express()
-const router = express.Router()
-
-const configResolver = require('./modules/configResolver.js')
+const router = require('./controllers')
 const version = 'v.0.0.12'
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-const mainController = require('./controllers/mainController')(router)
+//const mainController = require('./controllers/mainController')
 
 app.use('/api', router);
 
