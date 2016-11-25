d969722e5ff86848272251c867dc74c61f68625f
jacekelgda <jacekelgda@gmail.com>
2016-11-25 12:13:14 +0200

    campaignController, chatHandler - sending chat messages (#5)

--- app/modules/attachmentsResolver.js
+++ app/modules/attachmentsResolver.js
@@ -1,6 +1,10 @@
-function getAttachments() {
-  var resources_attachemnts = require('./resources/attachments.json');
+getAttachments = (campaignId) => {
+  resources_attachemnts = require('./../resources/attachments.json');
   resources_attachemnts[0].callback_id = campaignId;
 
   return JSON.stringify(resources_attachemnts);
+}
+
+module.exports = {
+  getAttachments
 }
--- app/modules/chatHandler.js
+++ app/modules/chatHandler.js
@@ -1,7 +1,10 @@
 const Slack = require('slack-node')
 const configResolver = require('./configResolver')
-const listUsersMethod = 'users.list'
+const attachmentsResolver = require('./attachmentsResolver')
 const slack = new Slack(configResolver.getConfigVariable('API_TOKEN'))
+
+const listUsersMethod = 'users.list'
+const chatPostMessageMethod = 'chat.postMessage'
 
 getUsersList = (report) => {
   return new Promise((resolve, reject) => {
@@ -9,13 +12,18 @@ getUsersList = (report) => {
       if (err) {
         reject(err)
       } else {
-        resolve({response: response, report: report})
+        if (report) {
+            resolve({response: response, report: report})
+        } else {
+            resolve({response: response})
+        }
       }
     })
   })
 }
 
 const getEncodedReport = (report) => getUsersList(report).then(encodeUsers)
+const sendMultipleMoodMessages = () => getUsersList().then(filterActiveUsers).then(sendMoodMessages)
 
 encodeUsers = (response) => {
   encodedReport = []
@@ -32,6 +40,52 @@ encodeUsers = (response) => {
   return encodedReport
 }
 
+sendMoodMessage = (campaignId, user) => {
+  attachments = attachmentsResolver.getAttachments(campaignId)
+  return new Promise((resolve, reject) => {
+    slack.api(chatPostMessageMethod, {
+      username: configResolver.getConfigVariable('BOT_NAME'),
+      as_user: false,
+      channel: '@' + user,
+      icon_url: configResolver.getConfigVariable('BOT_ICON_URL'),
+      attachments: attachments
+    }, function(err, response) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(response)
+      }
+    })
+  })
+}
+
+sendMoodMessages = (users) => {
+  campaignId = 'C' + Date.now()
+  for (var index in users) {
+    sendMoodMessage(campaignId, users[index])
+  }
+}
+
+filterActiveUsers = (data) => {
+  users = []
+  for(var index in data.response.members){
+    if(!data.response.members[index].is_bot && !data.response.members[index].deleted) {
+      slackUsername = data.response.members[index].name;
+
+      if (!configResolver.getConfigVariable('IS_PROD_ENV')) {
+        if (slackUsername === configResolver.getConfigVariable('TEST_USER')) {
+          users.push(slackUsername)
+        }
+      } else {
+        users.push(slackUsername)
+      }
+    }
+  }
+  return users
+}
+
 module.exports = {
-  getEncodedReport
+  getEncodedReport,
+  sendMoodMessage,
+  sendMultipleMoodMessages
 }
