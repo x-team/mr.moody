a038e9d2c6f8aa8b0ba0f3887c56b0b0d0ccaebd
Jacek Lawniczak <jacekelgda@gmail.com>
2016-11-25 13:53:02 +0200

    router refactor

---
+++ app/controllers/interactiveMessagesController.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const router = new express.Router()
+
+router.post('/votes', function (req, res) {
+	console.log('/votes')
+})
+
+module.exports = router
+
+// if (req.body.payload) {
+// 	var payload = JSON.parse(req.body.payload);
+//
+// 	if (payload.token == configResolver.getConfigVariable('VERIFICATION_TOKEN')) {
+// 		var mood = payload.actions[0].value;
+// 		var id = payload.user.id;
+// 		var campaign = payload.callback_id;
+// 		//writeVoteData(id, mood, campaign);
+// 	} else {
+// 		console.log('token is not valid');
+// 	}
+//
+// 	var message = {
+// 		"text": "Thank you for your answer. Have a great weekend!",
+// 	};
+// 	res.json(message);
+//
+// } else {
+// 	res.send('Invalid param');
+// }
