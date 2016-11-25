5532c89333de6e06bdb9eb8574a521ecc3ef1be1
jacekelgda <jacekelgda@gmail.com>
2016-11-25 12:13:14 +0200

    campaignController, chatHandler - sending chat messages (#5)

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,37 +1,37 @@
 const validationMiddleware = require('./middleware/validationMiddleware')
 const errorMiddleware = require('./middleware/errorMiddleware')
 const reportsController = require('./reportsController')
+const campaignController = require('./campaignController')
 const express = require('express')
 const router = new express.Router()
 
 router.use(validationMiddleware.validateRequest)
 router.use(errorMiddleware.errorMiddleware)
+
 router.post('/slack-command', function (req, res) {
   action = req.body.text
 
-  // extract campaign report
   isReport = /^campaign\:report\:.*/.test(action)
   if (isReport) {
     command = action.substring(0, ('campaign:report').length)
-
   } else {
     command = action
   }
-  //
 
   switch (command) {
+    case "campaign:test":
+      campaignController.sendChatMessageToTestUser()
+      res.send('Starting campaign')
+      break;
     case "campaign:start":
-      console.log('starting campaign ...')
+      campaignController.sendChatMessageToTestUsers()
       res.send('Starting campaign')
       break;
     case "campaign:list":
-      console.log('showing campaign list ...')
       reportsController.generateCampaignsReport(req, res);
       break;
     case "campaign:report":
-      // extract campaign id
       campaignId = action.substring(('campaign:report:').length, action.length)
-      //
       reportsController.generateCampaignReport(campaignId, req, res)
       break;
     default:
