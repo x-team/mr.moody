7259d6f1161a72593026cd1b214ae5c07386b910
jacekelgda <jacekelgda@gmail.com>
2017-10-03 13:54:23 +0200

    Refactoring of chat handler

--- handler/api/slack.js
+++ handler/api/slack.js
@@ -1,4 +1,5 @@
 import Botkit from 'botkit'
+import { getAttachments } from './../../resolver/attachments'
 
 const controller = Botkit.slackbot()
 const bot = controller.spawn({
@@ -23,7 +24,7 @@ const getUsers = (report) => {
 
 const filterActiveUsers = (data) => {
   let users = []
-  for(var index in data.response.members){
+  for (var index in data.response.members){
     const userData = data.response.members[index]
     if(!userData.is_bot && !userData.deleted && !userData.is_restricted) {
       const slackUsername = data.response.members[index].name;
@@ -40,7 +41,42 @@ const filterActiveUsers = (data) => {
   return users
 }
 
+const sendMoodMessage = (campaignId, user) => {
+  const attachments = getAttachments(campaignId)
+  return new Promise((resolve, reject) => {
+    slackBot.api(chatPostMessageMethod, {
+      username: process.env.BOT_NAME,
+      as_user: true,
+      channel: '@' + user,
+      attachments: attachments
+    }, function(err, response) {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(response)
+      }
+    })
+  })
+}
+
+const encodeUsers = (response) => {
+  let encodedReport = []
+  for(var index in response.response.members) {
+    if(!response.response.members[index].is_bot
+      && !response.response.members[index].deleted
+      && response.report[response.response.members[index].id]) {
+        encodedReport.push({
+          name: response.response.members[index].name,
+          mood: response.report[response.response.members[index].id]
+        })
+      }
+  }
+  return encodedReport
+}
+
 export {
   getUsers,
-  filterActiveUsers
+  filterActiveUsers,
+  sendMoodMessage,
+  encodeUsers
 }
--- handler/chat.js
+++ handler/chat.js
@@ -1,9 +1,4 @@
-import Slack from 'slack-node'
-import attachmentsResolver from './../resolver/attachments'
-import { getUsers, filterActiveUsers } from './api/slack'
-
-const slack = new Slack(process.env.API_TOKEN)
-const slackBot = new Slack(process.env.BOT_TOKEN)
+import { getUsers, filterActiveUsers, encodeUsers, sendMoodMessage } from './api/slack'
 
 const listUsersMethod = 'users.list'
 const chatPostMessageMethod = 'chat.postMessage'
@@ -11,39 +6,6 @@ const chatPostMessageMethod = 'chat.postMessage'
 const getEncodedReport = (report) => getUsersList(report).then(encodeUsers)
 const sendMultipleMoodMessages = () => getUsersList().then(filterActiveUsers).then(sendMoodMessages)
 const testUserGroup = () => getUsers().then(filterActiveUsers).then(printUsers)
-
-const encodeUsers = (response) => {
-  encodedReport = []
-  for(var index in response.response.members) {
-    if(!response.response.members[index].is_bot
-      && !response.response.members[index].deleted
-      && response.report[response.response.members[index].id]) {
-        encodedReport.push({
-          name: response.response.members[index].name,
-          mood: response.report[response.response.members[index].id]
-        })
-      }
-  }
-  return encodedReport
-}
-
-const sendMoodMessage = (campaignId, user) => {
-  attachments = attachmentsResolver.getAttachments(campaignId)
-  return new Promise((resolve, reject) => {
-    slackBot.api(chatPostMessageMethod, {
-      username: process.env.BOT_NAME,
-      as_user: true,
-      channel: '@' + user,
-      attachments: attachments
-    }, function(err, response) {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(response)
-      }
-    })
-  })
-}
 
 const sendMoodMessages = (users) => {
   campaignId = 'C' + Date.now()
@@ -58,7 +20,6 @@ const printUsers = (users) => {
 
 export {
   getEncodedReport,
-  sendMoodMessage,
   sendMultipleMoodMessages,
   testUserGroup
 }
